9cd7e4a39f9aca0f90d0c7849024e84d75e4a59d
chzhkv <chizhikvo@gmail.com>
2024-07-02 22:29:08 +0400

    Change functions and add tests for hw7

--- src/hw7/addNewParagraph.js
+++ src/hw7/addNewParagraph.js
@@ -1,7 +1,6 @@
-const input = document.querySelector('.input');
-const button = document.querySelector('.btn');
-
 export const addNewParagraph = () => {
+    const input = document.querySelector('.input');
+    const button = document.querySelector('.btn');
     const newParagraph = document.createElement('p');
     newParagraph.innerText = input.value;
     const paragraphBox = document.querySelector('.box');
