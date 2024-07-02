db6b7e382c11a63079bc2ae1a01d3105465fa5b8
chzhkv <chizhikvo@gmail.com>
2024-07-02 17:40:25 +0400

    Change functions and add tests for hw1

--- src/hw1/script1.js
+++ src/hw1/script1.js
@@ -1,20 +1,7 @@
-const a = 5;
-const b = 7;
-console.log ('Задача 1: ' + (a + b), (a * b));
+import {sumAndMult} from "./sumAndMult.js";
+import {lengthSum} from "./lengthSum.js";
+import {threeDigitsSum} from "./threeDigitsSum.js";
 
-const c = 'test';
-const d = 'text';
-console.log ('Задача 2: ' + (c.length + d.length));
-
-let input;
-
-do {
-    input = +prompt('Введите 3-х значное число');
-} while (!((input <= 999) && (input >= 100)));
-
-let edinitcy = input % 10;
-let desyatki = ((input % 100) - edinitcy) / 10;
-let sotni = (input - (input % 100)) / 100;
-
-console.log(`Введеное число: ${input}. Результат сложения ${sotni} + ${desyatki} + ${edinitcy} = ` + (sotni + desyatki + edinitcy));
-
+sumAndMult(5, 7);
+lengthSum('test', 'pet');
+threeDigitsSum();
